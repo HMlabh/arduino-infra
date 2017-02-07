fd346f366ddf3da709282bea20ea8003f9408154
Metheus101 <gutekunstmarkus@gmx.net>
2017-02-07 15:17:18 +0100

    readall fastread updated Libary

--- Debug/arduino-infra.cpp.d
+++ Debug/arduino-infra.cpp.d
@@ -13,4 +13,8 @@ C:\Users\gutek\AppData\Local\Temp\VMBuilds\arduino-infra\micro\Debug\arduino-inf
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBDesc.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBCore.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\micro/pins_arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\micro/../leonardo/pins_arduino.h
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\micro/../leonardo/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\gutek\Documents\Arduino\libraries\Adafruit_ADS1X15-master/Adafruit_ADS1015.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h
--- arduino-infra.ino
+++ arduino-infra.ino
@@ -4,7 +4,8 @@
  Author:	gutek
 */
 
-
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
 //------------------Defines-------------------
 #define serialDebug //Debug output over Serial connection to PC
 
@@ -48,6 +49,15 @@ int enables[16] =
   pin::EN10 , pin::EN11 , pin::EN12 , pin::EN13 , pin::EN14 , pin::EN15};
 
 int measurecount = 0;
+long measuretime = 0;
+
+int16_t ranges[16] = { 0 };
+
+//-----ADC-----
+Adafruit_ADS1115 ad0(addr::AD0);	// construct an ads1115 at address AD0
+Adafruit_ADS1115 ad1(addr::AD1);	// construct an ads1115 at address AD1
+Adafruit_ADS1115 ad2(addr::AD2);	// construct an ads1115 at address AD2
+Adafruit_ADS1115 ad3(addr::AD3);	// construct an ads1115 at address AD3
 
 //-------------------Setup-----------------------
 
@@ -66,22 +76,158 @@ void setup()
 	//Pinmode Enables
 	for (int i = 0; i <= 15; i++)
 	{
-		pinMode(enables[], OUTPUT); //Sensor 0 Enable 0utput
+		pinMode(enables[i], OUTPUT); //Sensor i Enable 0utput
 	}
+	
+	// HACK SEN ON!!!
+	for (int i = 0; i <= 15; i++)
+	{
+		digitalWrite(enables[i], HIGH); //Sensor i HIGH
+	}
+
+	//Begin serial communication to ADS1115
+	ad0.begin();
+	ad1.begin();
+	ad2.begin();
+	ad3.begin();
+
 	
 
 }
 
 //-------------------Functions------------------
 
+int16_t readsensor(int sensornr)
+{
+	int16_t range = 0;
 
+	if (sensornr >= 0 && sensornr <= 15)
+	{
+		switch (sensornr)
+		{
 
+		case 0:range = ad0.readADC_SingleEnded(0);break;
+		case 1:range = ad0.readADC_SingleEnded(1);break;
+		case 2:range = ad0.readADC_SingleEnded(2);break;
+		case 3:range = ad0.readADC_SingleEnded(3);break;
+
+		case 4:range = ad1.readADC_SingleEnded(0);break;
+		case 5:range = ad1.readADC_SingleEnded(1); break;
+		case 6:range = ad1.readADC_SingleEnded(2); break;
+		case 7:range = ad1.readADC_SingleEnded(3); break;
+
+		case 8:range = ad2.readADC_SingleEnded(0); break;
+		case 9:range = ad2.readADC_SingleEnded(1); break;
+		case 10:range = ad2.readADC_SingleEnded(2); break;
+		case 11:range = ad2.readADC_SingleEnded(3); break;
+
+		case 12:range = ad3.readADC_SingleEnded(0); break;
+		case 13:range = ad3.readADC_SingleEnded(1); break;
+		case 14:range = ad3.readADC_SingleEnded(2); break;
+		case 15:range = ad3.readADC_SingleEnded(3); break;
+		}
+		return range;
+	}
+	else
+	{
+		return -9999;
+	}
+}
+
+void readallsensors()
+{
+	ranges[0] = ad0.readADC_SingleEnded(0);
+	ranges[1] = ad0.readADC_SingleEnded(1);
+	ranges[2] = ad0.readADC_SingleEnded(2);
+	ranges[3] = ad0.readADC_SingleEnded(3);
+
+	ranges[4] = ad1.readADC_SingleEnded(0);
+	ranges[5] = ad1.readADC_SingleEnded(1);
+	ranges[6] = ad1.readADC_SingleEnded(2);
+	ranges[7] = ad1.readADC_SingleEnded(3);
+
+	ranges[8] = ad2.readADC_SingleEnded(0);
+	ranges[9] = ad2.readADC_SingleEnded(1);
+	ranges[10] = ad2.readADC_SingleEnded(2);
+	ranges[11] = ad2.readADC_SingleEnded(3);
+
+	ranges[12] = ad3.readADC_SingleEnded(0);
+	ranges[13] = ad3.readADC_SingleEnded(1);
+	ranges[14] = ad3.readADC_SingleEnded(2);
+	ranges[15] = ad3.readADC_SingleEnded(3);
+}
+
+void fastread()
+{
+	//Start Reading Block 0:
+	ad0.readADC_SingleEnded_Start(0);
+	ad1.readADC_SingleEnded_Start(0);
+	ad2.readADC_SingleEnded_Start(0);
+	ad3.readADC_SingleEnded_Start(0);
+	delay(8);
+	//Read Block 0
+	ranges[0]=ad0.readADC_SingleEnded_Read(0);
+	ranges[4] = ad0.readADC_SingleEnded_Read(0);
+	ranges[8] = ad0.readADC_SingleEnded_Read(0);
+	ranges[12] = ad0.readADC_SingleEnded_Read(0);
+
+	//Start Reading Block 1:
+	ad0.readADC_SingleEnded_Start(1);
+	ad1.readADC_SingleEnded_Start(1);
+	ad2.readADC_SingleEnded_Start(1);
+	ad3.readADC_SingleEnded_Start(1);
+	delay(8);
+	//Read Block 1
+	ranges[1] = ad0.readADC_SingleEnded_Read(1);
+	ranges[5] = ad0.readADC_SingleEnded_Read(1);
+	ranges[9] = ad0.readADC_SingleEnded_Read(1);
+	ranges[13] = ad0.readADC_SingleEnded_Read(1);
+
+	//Start Reading Block 2:
+	ad0.readADC_SingleEnded_Start(2);
+	ad1.readADC_SingleEnded_Start(2);
+	ad2.readADC_SingleEnded_Start(2);
+	ad3.readADC_SingleEnded_Start(2);
+	delay(8);
+	//Read Block 2
+	ranges[2] = ad0.readADC_SingleEnded_Read(2);
+	ranges[6] = ad0.readADC_SingleEnded_Read(2);
+	ranges[10] = ad0.readADC_SingleEnded_Read(2);
+	ranges[14] = ad0.readADC_SingleEnded_Read(2);
+
+	//Start Reading Block 3:
+	ad0.readADC_SingleEnded_Start(3);
+	ad1.readADC_SingleEnded_Start(3);
+	ad2.readADC_SingleEnded_Start(3);
+	ad3.readADC_SingleEnded_Start(3);
+	delay(8);
+	//Read Block 3
+	ranges[3] = ad0.readADC_SingleEnded_Read(3);
+	ranges[7] = ad0.readADC_SingleEnded_Read(3);
+	ranges[11] = ad0.readADC_SingleEnded_Read(3);
+	ranges[15] = ad0.readADC_SingleEnded_Read(3);
+
+}
 
 //-------------------Loop--------------------
 
 void loop() 
 {
 	#ifdef serialDebug
+
+	measuretime = micros();
+
+	fastread();
+	//readallsensors();
+	measuretime = micros() - measuretime;
+
+	for (int i = 0; i <= 15; i++)
+	{
+		Serial.println(String("Sensor ")+(i)+(" : ")+(ranges[i]));
+	}
+	Serial.println(measuretime);
+	Serial.println("------------------------------");
+
 
 	#endif // serialDebug
 
