784a09427753b106d46430174891066bec406825
Metheus101 <gutekunstmarkus@gmx.net>
2017-02-07 11:36:30 +0100

    cleanup + pinset

--- arduino-infra.ino
+++ arduino-infra.ino
@@ -1,6 +1,6 @@
 /*
- Name:		arduino-ultra.ino
- Created:	06.02.2017 14:40:44
+ Name:		arduino-infra.ino
+ Created:	07.02.2017 14:40:44
  Author:	gutek
 */
 
@@ -8,60 +8,45 @@
 //------------------Defines-------------------
 #define serialDebug //Debug output over Serial connection to PC
 
-long conversionfactor = 29.1;
-unsigned long measuredelay = 19500;
-long measuretimeout = 19000;
-
-long measurecompletetime = 0;
-long sendcompletetime = 0;
 
 
 //------------------Pinset--------------------
 
-#define test A1
-
 namespace pin
 {
-	int sen0_t = 1;
-	int sen0_e = 0;
-	int sen1_t = 2;
-	int sen1_e = 3;
-	int sen2_t = 4;
-	int sen2_e = 5;
-	int sen3_t = 6;
-	int sen3_e = 7;
-	int sen4_t = 8;
-	int sen4_e = 9;
-	int sen5_t = 10;
-	int sen5_e = 11;
-	int sen6_t = 12;
-	int sen6_e = 13;
-	int sen7_t = 18;
-	int sen7_e = 19;
-	int sen8_t = 20;
-	int sen8_e = 21;
-	int sen9_t = 22;
-	int sen9_e = 23;
+	int SCL = 3;
+	int SDA = 2;
+	int EN0 = 11;
+	int EN1 = 10;
+	int EN2 = 9;
+	int EN3 = 8;
+	int EN4 = 7;
+	int EN5 = 6;
+	int EN6 = 5;
+	int EN7 = 4;
+	int EN8 = 19;
+	int EN9 = 18;
+	int EN10 = 13;
+	int EN11 = 12;
+	int EN12 = 23;
+	int EN13 = 22;
+	int EN14 = 21;
+	int EN15 = 20;
+}
+namespace addr
+{
+	int AD0 = 0x48;
+	int AD1 = 0x49;
+	int AD2 = 0x4A;
+	int AD3 = 0x4B;
 }
 
-
 //-------------------Init-----------------------
+int enables[16] =
+{ pin::EN0 , pin::EN1 , pin::EN2 , pin::EN3 , pin::EN4 , 
+  pin::EN5 , pin::EN6 , pin::EN7 , pin::EN8 , pin::EN9 , 
+  pin::EN10 , pin::EN11 , pin::EN12 , pin::EN13 , pin::EN14 , pin::EN15};
 
-int sensor[10][2] =				// Array sensor [Sensornummer][Trigger,Echo]
-{
-{ pin::sen0_t , pin::sen0_e }, //Line 0 : Sensor 0 [T,E]
-{ pin::sen1_t , pin::sen1_e }, //Line 1 : Sensor 1 [T,E]
-{ pin::sen2_t , pin::sen2_e }, //Line 2 : Sensor 2 [T,E]
-{ pin::sen3_t , pin::sen3_e }, //Line 3 : Sensor 3 [T,E]
-{ pin::sen4_t , pin::sen4_e }, //Line 4 : Sensor 4 [T,E]
-{ pin::sen5_t , pin::sen5_e }, //Line 5 : Sensor 5 [T,E]
-{ pin::sen6_t , pin::sen6_e }, //Line 6 : Sensor 6 [T,E]
-{ pin::sen7_t , pin::sen7_e }, //Line 7 : Sensor 7 [T,E]
-{ pin::sen8_t , pin::sen8_e }, //Line 8 : Sensor 8 [T,E]
-{ pin::sen9_t , pin::sen9_e }  //Line 9 : Sensor 9 [T,E]
-};
-
-long ranges[10] = { 0 };		//Rangevector ; Line = Sensornumber
 int measurecount = 0;
 
 //-------------------Setup-----------------------
@@ -70,60 +55,26 @@ void setup()
 {
 #ifdef serialDebug				//establish Serial Debug-Connection
 	Serial.begin(115200);
-	
 	while (!Serial) 
 	{
 		; // wait for serial port to connect. Needed for native USB port only
 	}
-	
 #endif // serialDebug
 
 
 
-	//Pinmode Sensors
-	for (int n = 0; n <= 9; n++)
+	//Pinmode Enables
+	for (int i = 0; i <= 15; i++)
 	{
-		pinMode(sensor[n][0], OUTPUT); //Sensor n Trigger -> Output
-		pinMode(sensor[n][1], INPUT_PULLUP);  //Sensor n Echo    -> Output
+		pinMode(enables[], OUTPUT); //Sensor 0 Enable 0utput
 	}
+	
+
 }
 
 //-------------------Functions------------------
 
-long getrange(int sensornumber)
-{
-	long range = 0;
-	long time = 0;
-	
-	unsigned long measuretime = micros();
-	digitalWrite(sensor[sensornumber][0], LOW); //trigger -> LOW
-	delayMicroseconds(3);
-	noInterrupts();
-	digitalWrite(sensor[sensornumber][0], HIGH); //Trigger Impuls 10 us
-	delayMicroseconds(10);
-	digitalWrite(sensor[sensornumber][0], LOW);
-	time = pulseIn(sensor[sensornumber][1], HIGH,measuretimeout); // Echo-Zeit messen
-	interrupts();
-	time = (time / 2); // Zeit halbieren
-	range = time / conversionfactor; // Zeit in Zentimeter umrechnen
-	if (micros() > measuretime)		//Check if micros overflowed
-	{
-		while (micros() <= measuretime + measuredelay)
-		{
-		}
-	}
-	return(range);
-}
 
-#ifdef serialDebug
-void displayranges()
-{
-	for (int i = 0; i <= 9; i++)
-	{
-		Serial.println(ranges[i]);
-	}
-}
-#endif // serialDebug
 
 
 //-------------------Loop--------------------
@@ -131,18 +82,7 @@ void displayranges()
 void loop() 
 {
 	#ifdef serialDebug
-	measurecompletetime = micros();
-	for (int n = 0; n <= 9; n++)
-	{
-		ranges[n] = getrange(n);
-	}
-	measurecompletetime = micros() - measurecompletetime;
-	sendcompletetime = micros();
 
-	displayranges();
-
-	sendcompletetime = micros() - sendcompletetime;
-	Serial.println(String(measurecount) + ("|m-time:") + (measurecompletetime)+("|s-time:") + (sendcompletetime));
 	#endif // serialDebug
 
 	
